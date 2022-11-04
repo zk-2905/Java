cbb093fad674c16a085312f9beb3a51c8418bcc4
zk-2905 <113169587+zk-2905@users.noreply.github.com>
2022-11-04 10:31:02 +0000

    Add files via upload

---
+++ Challenge2.java
@@ -0,0 +1,9 @@
+public class Challenge2 {
+    public static void main(String[] args){
+        int total = 0;
+        for(int i = 0; i < 11; i++ ) {
+            total += i;
+        }
+        System.out.println(total);
+    }
+}
